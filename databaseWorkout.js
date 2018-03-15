645e6e29c88a62fa742226301c7c884c50f3f9f1
youngroz <garzar@oregonstate.edu>
2018-03-15 11:26:19 -0700

    attempt fix

--- databaseWorkout.js
+++ databaseWorkout.js
@@ -172,14 +172,12 @@ app.get('/insertToCompany', function(req, res, next) {
                           "INNER JOIN company c ON c.id = bt.co_id " +
                           "WHERE p.id=? AND c.id=? ";
 
-      pool.query(poiCoString, [req.query.per_id, req.query.co_id], function(err, rows, result) {
+      pool.query(poiCoString, [req.query.per_id, req.query.co_id], function(err, result) {
         if (err) {
           next(err);
           return;
         }
-        for (var i in rows) {
-          console.log(rows[i].co_name)
-        }
+
         res.send(JSON.stringify(result[0]));
       });
     }
