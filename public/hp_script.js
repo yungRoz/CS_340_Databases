4afbac7fa5d1fdbddbf71deed04454f69412578a
youngroz <garzar@oregonstate.edu>
2018-03-18 05:42:11 -0700

    remove modal tests

--- public/hp_script.js
+++ public/hp_script.js
@@ -1,26 +1,4 @@
 document.addEventListener('DOMContentLoaded', bindAddPersonButton);
-//document.addEventListener('DOMContentLoadedHP', bindHomePageButton);
-var opener = document.getElementById("opener");
-opener.onclick = function() {
-  var lightbox = document.getElementById("addToCompanyModal"),
-    dimmer = document.createElement("div");
-
-  dimmer.style.width = window.innerWidth + 'px';
-  dimmer.style.height = window.innerHeight + 'px';
-  dimmer.className = 'dimmer';
-
-  dimmer.onclick = function() {
-    document.body.removeChild(this);
-    lightbox.style.visibility = 'hidden';
-  }
-
-  document.body.appendChild(dimmer);
-
-  lightbox.style.visibility = 'visible';
-  lightbox.style.top = window.innerHeight / 2 - 50 + 'px';
-  lightbox.style.left = window.innerWidth / 2 - 100 + 'px';
-  return false;
-}
 
 function bindAddPersonButton() {
   document.querySelector("#addToCompany").addEventListener('submit', function(event) {
